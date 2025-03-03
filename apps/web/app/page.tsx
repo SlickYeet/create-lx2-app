6d6bcc85843c61ea2dbe26c840435b93d9d6c9ee
SlickYeet <lasse@famlam.ca>
2025-03-03 01:37:24 -0800

    added shadcn to web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button"
+
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col gap-y-2 items-center justify-center h-screen">
       <h1>Welcome to the homepage!</h1>
+      <Button asChild>
+        <Link href="/docs">Get started</Link>
+      </Button>
     </div>
   )
 }
