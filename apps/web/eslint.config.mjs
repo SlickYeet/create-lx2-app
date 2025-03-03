37994ff11407d30ea894acaea857baba27a6da96
SlickYeet <lasse@famlam.ca>
2025-03-02 23:56:02 -0800

    init

---
+++ apps/web/eslint.config.mjs
@@ -0,0 +1,20 @@
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
+
+import { nextJsConfig } from "@repo/eslint-config/next-js"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+/** @type {import("eslint").Linter.Config} */
+const eslintConfig = [
+  nextJsConfig,
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+]
+
+export default eslintConfig
