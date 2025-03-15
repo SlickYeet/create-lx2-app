844262021bf5a13255c41dbe54ace60f32f5cc12
SlickYeet <lasse@famlam.ca>
2025-03-15 15:04:33 -0700

    feat: restructure project template

--- cli/template/base/src/app/page.tsx
+++ cli/template/base/src/app/page.tsx
@@ -1,114 +1,106 @@
-import Image from "next/image"
+import Link from "next/link"
 
 export default function HomePage() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <div className="flex items-center gap-2">
+    <main className="flex min-h-screen flex-col items-center justify-center px-4 text-center">
+      {/* Logo */}
+      <div className="mb-8">
+        <div className="relative flex h-24 w-24 items-center justify-center">
+          <div className="absolute inset-0 animate-pulse rounded-xl bg-gradient-to-r from-purple-500 to-cyan-500 opacity-20 blur-xl dark:from-purple-800 dark:to-cyan-800" />
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="50"
-            height="50"
+            width="80"
+            height="80"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            className="from-primary/50 to-accent/50 rounded-lg bg-linear-to-r"
+            className="rounded-lg bg-gradient-to-r from-purple-500 to-cyan-500 dark:from-purple-800 dark:to-cyan-800"
           >
             <polyline points="4 17 10 11 4 5" />
             <line x1="12" x2="20" y1="19" y2="19" />
           </svg>
-          <span className="inline-block text-4xl font-bold">
-            TNT-Powered Base Template
-          </span>
         </div>
+      </div>
 
-        <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="rounded bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <div className="max-w-xl text-balance">
+        <h1 className="mb-8 bg-gradient-to-r from-purple-500 to-cyan-500 bg-clip-text pb-1.5 text-6xl font-bold tracking-tighter text-transparent md:text-7xl lg:text-8xl">
+          TNT-Powered Next.js App
+        </h1>
+        <p className="mb-12 text-xl text-neutral-700 md:text-2xl dark:text-neutral-300">
+          Build modern web applications with todat&apos;s most popular tools
+        </p>
+      </div>
 
-        <div className="flex flex-col items-center gap-4 sm:flex-row">
-          <a
-            className="bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm transition-colors hover:bg-[#383838] sm:h-12 sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://create.tntstack.org/docs"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://create.tntstack.org/docs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://create.tntstack.org/docs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+      <div className="mb-12 flex flex-col gap-10 sm:flex-row">
+        <Link
           href="https://create.tntstack.org"
           target="_blank"
-          rel="noopener noreferrer"
+          referrerPolicy="no-referrer"
+          className="hover:text-primary relative flex items-center justify-center gap-2 text-lg font-medium"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to create.tntstack.org →
-        </a>
-      </footer>
-    </div>
+          Website
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="absolute top-0 -right-4 size-4 fill-none stroke-current stroke-2"
+          >
+            <path d="M7 7h10v10" />
+            <path d="M7 17 17 7" />
+          </svg>
+        </Link>
+
+        <Link
+          href="https://create.tntstack.org/docs"
+          target="_blank"
+          referrerPolicy="no-referrer"
+          className="hover:text-primary relative flex items-center justify-center gap-2 text-lg font-medium"
+        >
+          Docs
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="absolute top-0 -right-4 size-4 fill-none stroke-current stroke-2"
+          >
+            <path d="M7 7h10v10" />
+            <path d="M7 17 17 7" />
+          </svg>
+        </Link>
+
+        <Link
+          href="https://github.com/SlickYeet/create-tnt-stack"
+          target="_blank"
+          referrerPolicy="no-referrer"
+          className="hover:text-primary relative flex items-center justify-center gap-2 text-lg font-medium"
+        >
+          GitHub
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="absolute top-0 -right-4 size-4 fill-none stroke-current stroke-2"
+          >
+            <path d="M7 7h10v10" />
+            <path d="M7 17 17 7" />
+          </svg>
+        </Link>
+      </div>
+
+      <div className="mt-16 text-sm text-neutral-600 dark:text-neutral-400">
+        <p>
+          Get started by editing{" "}
+          <code className="rounded-md bg-neutral-200 px-2 py-1 dark:bg-neutral-800">
+            src/app/page.tsx
+          </code>
+        </p>
+      </div>
+    </main>
   )
 }
