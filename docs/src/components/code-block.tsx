ea2a9d74733e4aebb6dd28ddeeab5b2057cc85fb
SlickYeet <lasse@famlam.ca>
2025-03-27 13:40:09 -0700

    feat(docs): replace title div with Figcaption component in CodeBlock for improved semantics

--- docs/src/components/code-block.tsx
+++ docs/src/components/code-block.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 
 import { Pre, PreProps } from "@/components/mdx/pre"
 import { cn, processCode } from "@/lib/utils"
+import { Figcaption } from "@/mdx-components"
 
 interface CodeBlockProps extends PreProps {
   code: string
@@ -27,11 +28,7 @@ export function CodeBlock({
 
   return (
     <div className={cn("mdx", className)}>
-      {title && (
-        <div className="bg-muted/80 border-muted -mb-6 min-w-sm rounded-t-lg border px-5 py-2">
-          {title}
-        </div>
-      )}
+      {title && <Figcaption>{title}</Figcaption>}
       <Pre
         code={code}
         data-language={language}
