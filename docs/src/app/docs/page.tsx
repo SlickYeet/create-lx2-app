197ab3b19b35f6dd3fd0b81776b9bbe7675bf2ce
SlickYeet <lasse@famlam.ca>
2025-04-02 19:28:57 -0700

    chore(docs): add comment to docs page

--- docs/src/app/docs/page.tsx
+++ docs/src/app/docs/page.tsx
@@ -3,5 +3,9 @@ import { redirect } from "next/navigation"
 import { RELATIVE_INITIAL_DOCS_PATH } from "@/constants"
 
 export default function DocsPage() {
+  /**
+   * ! This is a temporary solution until I build a proper landing page for the docs
+   * Redirects to the initial docs page (/docs/introduction)
+   */
   return redirect(RELATIVE_INITIAL_DOCS_PATH)
 }
