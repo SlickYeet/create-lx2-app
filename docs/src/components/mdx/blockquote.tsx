800c619185ea0aef6738dd6aa325c35b5b075c93
SlickYeet <lasse@famlam.ca>
2025-06-28 22:55:28 -0700

    feat(docs): add Blockquote component

---
+++ docs/src/components/mdx/blockquote.tsx
@@ -0,0 +1,15 @@
+import type { ComponentProps } from "react"
+
+import { cn } from "@/lib/utils"
+
+export function Blockquote({
+  className,
+  ...props
+}: ComponentProps<"blockquote">) {
+  return (
+    <blockquote
+      className={cn("border-primary mt-6 border-l-2 pl-6 italic", className)}
+      {...props}
+    />
+  )
+}
