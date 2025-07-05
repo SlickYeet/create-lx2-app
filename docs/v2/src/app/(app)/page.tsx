8991154359485a0542ea21f3a600e68b2de39631
SlickYeet <lasse@famlam.ca>
2025-07-04 21:20:27 -0700

    feat(docs): update home page

--- docs/v2/src/app/(app)/page.tsx
+++ docs/v2/src/app/(app)/page.tsx
@@ -1,9 +1,24 @@
+import { CTA } from "@/components/home/cta"
+import { Features } from "@/components/home/features"
+import { Hero } from "@/components/home/hero"
+import { Usage } from "@/components/home/usage"
+import { Footer } from "@/components/layout/footer"
+import { Header } from "@/components/layout/header"
+import { getNpmVersion } from "@/lib/utils"
+
 export default async function HomePage() {
+  const npmVersion = await getNpmVersion("latest")
+
   return (
     <>
-      Header
-      <div className="flex min-h-screen flex-col">Content</div>
-      Footer
+      <Header />
+      <div className="flex min-h-screen flex-col">
+        <Hero npmVersion={npmVersion} />
+        <Features />
+        <Usage />
+        <CTA />
+      </div>
+      <Footer />
     </>
   )
 }
