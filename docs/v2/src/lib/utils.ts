6a6095463aea62b130d8fe1ab81b506ea2bd4115
SlickYeet <lasse@famlam.ca>
2025-07-04 20:33:45 -0700

    feat(docs): add lib files

---
+++ docs/v2/src/lib/utils.ts
@@ -0,0 +1,36 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+import { env } from "@/env"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function absoluteUrl(path: string) {
+  return `${env.NEXT_PUBLIC_URL}${path}`
+}
+
+export async function getNpmVersion(
+  version: "latest" | "beta" = "latest",
+): Promise<string> {
+  // Fetch the latest version of the package from npm
+  // If the beta flag is true, fetch the beta version
+
+  const response = await fetch("https://registry.npmjs.org/create-tnt-stack", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  if (!response.ok) {
+    throw new Error("Failed to fetch the latest version")
+  }
+  const data = await response.json()
+
+  if (version === "latest") {
+    return data["dist-tags"].latest
+  } else {
+    return data["dist-tags"].beta
+  }
+}
