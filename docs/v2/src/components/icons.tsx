1d4a2538aeddf0dc9184c984e5384ed089392b9e
SlickYeet <lasse@famlam.ca>
2025-08-03 00:14:17 -0700

    feat(docs/v2): update logo

--- docs/v2/src/components/icons.tsx
+++ docs/v2/src/components/icons.tsx
@@ -4,19 +4,21 @@ type IconProps = React.HTMLAttributes<SVGElement>
 
 export const Icons = {
   logo: (props: IconProps) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M12 19h8" />
-      <path d="m4 17 6-6-6-6" />
-    </svg>
+    <div className="bg-foreground flex items-center justify-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="background"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        {...props}
+      >
+        <path d="M12 19h8" />
+        <path d="m4 17 6-6-6-6" />
+      </svg>
+    </div>
   ),
   github: (props: IconProps) => (
     <svg
