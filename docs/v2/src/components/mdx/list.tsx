9807d8aa0077cf8c0f90ad4c26e7390f07cfb551
SlickYeet <lasse@famlam.ca>
2025-09-18 00:57:47 -0700

    fix(docs/v2): remove unnecessary margin from list components

--- docs/v2/src/components/mdx/list.tsx
+++ docs/v2/src/components/mdx/list.tsx
@@ -1,11 +1,11 @@
 import { cn } from "@/lib/utils"
 
 export function Ul({ className, ...props }: React.ComponentProps<"ul">) {
-  return <ul className={cn("my-6 ml-6 list-disc", className)} {...props} />
+  return <ul className={cn("ml-6 list-disc", className)} {...props} />
 }
 
 export function Ol({ className, ...props }: React.ComponentProps<"ol">) {
-  return <ol className={cn("my-6 ml-6 list-decimal", className)} {...props} />
+  return <ol className={cn("ml-6 list-decimal", className)} {...props} />
 }
 
 export function Li({ className, ...props }: React.ComponentProps<"li">) {
